25fa01ee88331258c7741e6bc886a9d1ab5cdaf1
Mongkol Tuntisukumarl <mingbiz@gmail.com>
2026-07-02 13:42:33 +0700

    3rd commit 2Jul'26 time 13.42pm.

---
+++ my-object.js
@@ -0,0 +1,40 @@
+//"Pizza", "Malatang", "Pad Thai", "Burger", "Sushi", "Medovik"
+let foodDatabase = [
+    {
+        id: 1,
+        name:"Pizza", 
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 2,
+        name:"Malatang", 
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 3,
+        name:"Burger", 
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 4,
+        name:"Sushi", 
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 5,
+        name:"Madovik", 
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: false,
+    },
+];
+console.log(foodDatabase);
+
